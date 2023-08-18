dd8eba6276007d88f90c6cd9f990db17882c5ed5
Erik M Baxstrom <baxstrem@gmail.com>
2023-08-18 11:43:28 -0700

    refactor with microdot webserver module

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from connection_manager import connect_wifi, open_socket, serve
+from connection_manager import connect_wifi
+from microdot import Microdot, Response, send_file
+
+app = Microdot()
+Response.default_content_type = 'text/html'
+
+@app.route('/')
+def index(request):
+    response = send_file('index.html')
+    return response
+
+@app.route('/upload', methods=['POST'])
+def fileUpload(request):
+    print('request object', request.body)
+    return 'Success!', 200
+
 
 try:
     print('runing')
     ip = connect_wifi()
-    connection = open_socket(ip)
-    serve(connection)
+    app.run(host=ip, port=80, debug=True)
+#     connection = open_socket(ip)
+#     serve(connection)
 except KeyboardInterrupt:
     print('broke')
-    # machine.reset()
+#     # machine.reset()
+
+
+

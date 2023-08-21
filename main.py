6a734dd441aae64f4bcc1d3799fb80bf91159e0d
Erik M Baxstrom <baxstrem@gmail.com>
2023-08-21 12:22:36 -0700

    rename file manager

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from connection_manager import connect_wifi
 from microdot import Microdot, Response, send_file
-from print_manager import print_manager
+from file_manager import file_manager
 from print_controller import print_controller
 
 app = Microdot()
 Response.default_content_type = 'text/html'
 print_controller = print_controller()
-print_manager = print_manager(print_controller)
+file_manager = file_manager(print_controller)
 
 @app.route('/')
 def index(request):
@@ -16,7 +16,7 @@ def index(request):
 @app.route('/upload', methods=['POST'])
 def fileUpload(request):
     # print('Content-Range, Disposition', request.headers['Content-Range'], request.headers['Content-Disposition'])
-    print_manager.add_to_print_file(request.body)
+    file_manager.add_to_print_file(request.body)
     blah = request.body
     return 'Success!', 200
 
@@ -24,11 +24,11 @@ def fileUpload(request):
 @app.route('/print')
 def startPrint(request):
     print('print route called')
-    print_manager.start_print()
+    file_manager.start_print()
     return 'Success!', 200
 
 try:
-    print('runing')
+    print('running')
     ip = connect_wifi()
     app.run(host=ip, port=80, debug=True)
 except KeyboardInterrupt:

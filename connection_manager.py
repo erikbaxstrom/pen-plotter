7149bfdb48acde24588193666d836d0f6bee0b8c
Erik M Baxstrom <baxstrem@gmail.com>
2023-08-21 14:54:19 -0700

    remove unused code

--- connection_manager.py
+++ connection_manager.py
@@ -2,8 +2,6 @@ import network
 import socket
 from secrets import WIFI_SSID, WIFI_KEY
 from time import sleep
-from print_interface import run_stepper
-
 
 
 def connect_wifi():
@@ -18,34 +16,3 @@ def connect_wifi():
     print(wlan.ifconfig())
     ip = wlan.ifconfig()[0]
     return ip
-
-# def open_socket(ip):
-#     address = (ip, 80)
-#     connection = socket.socket()
-#     connection.bind(address)
-#     connection.listen(1)
-#     print(connection)
-#     return connection
-
-
-# def handle_request(request):
-#     try:
-#         split = request.split()
-#         path = split[1]
-#     except IndexError:
-#         return 'error'
-#     if path == '/runmotor?':
-#         run_stepper()
-
-
-
-# def serve(connection):
-#     while True:
-#         client = connection.accept()[0]
-#         request = client.recv(1024)
-#         request = str(request)
-#         print(request)
-#         handle_request(request)
-#         html = index()
-#         client.send(html)
-#         client.close()

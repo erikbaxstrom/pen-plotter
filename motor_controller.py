4a95e67800022008e3a32b26fdbf8bb156a2c6b0
Erik M Baxstrom <baxstrem@gmail.com>
2023-08-25 01:36:34 -0700

    implement negative steps and motor direction

--- motor_controller.py
+++ motor_controller.py
@@ -7,17 +7,15 @@ from pio_stepper import pio_step
 
 class motor_controller:
 
-    def __init__(self, base_pin, sm_number):
+    def __init__(self, base_pin, sm_number, motor_direction):
         print('init state machine. base pin, sm #', base_pin, sm_number)
-        # self.index = 0
-        self.pattern = int(('1000' + '0100' + '0010' + '0001') * 2, 2) # The bit mask pattern. Four x 4-bit masks, but state machine takes 32bit word, so double the sequence. Use base 2.
+        self.pattern = ('1000', '0100', '0010', '0001') * 2
+        self.motor_direction = motor_direction
         self.sm = StateMachine(sm_number, pio_step, freq=10000, set_base=Pin(base_pin), 
         out_base=Pin(base_pin))
-        # self.pacer = StateMachine(sm_number+1, pio_pace, freq=10000)
         self.sm.irq(self.busy_handler)
         self.is_busy = False
         self.sm.active(1)
-        # self.sm.restart()
 
     @property
     def is_busy(self):
@@ -38,12 +36,15 @@ class motor_controller:
     def step(self, steps):
         print('running this many steps:', steps)
         self.is_busy = True
-
+        steps *= self.motor_direction
+        pattern = self.pattern
+        if steps < 0:
+            pattern = list(reversed(pattern))
+            steps = -steps
+        bitmask = int( ''.join(pattern) , 2)
         self.sm.put(steps)
-        print('done put steps')
-        self.sm.put(self.pattern)
-        print('done put pattern')
-        # self.pacer.put(pace)
+        self.sm.put(bitmask)
+        print('done put steps, bitmask', bitmask, steps)
 
 
         # adjust index
@@ -70,14 +71,16 @@ LEFT_SM_BASE_PIN = 6
 RIGHT_SM_BASE_PIN = 2
 LEFT_SM_NUMBER = 0
 RIGHT_SM_NUMBER = 1
+LEFT_MOTOR_DIRECTION = -1
+RIGHT_MOTOR_DIRECTION = 1
 
-left_motor = motor_controller(LEFT_SM_BASE_PIN, LEFT_SM_NUMBER)
-right_motor = motor_controller(RIGHT_SM_BASE_PIN, RIGHT_SM_NUMBER)
+left_motor = motor_controller(LEFT_SM_BASE_PIN, LEFT_SM_NUMBER, LEFT_MOTOR_DIRECTION)
+right_motor = motor_controller(RIGHT_SM_BASE_PIN, RIGHT_SM_NUMBER, RIGHT_MOTOR_DIRECTION)
 # sleep(1)
 
 
 # run a test sequence (run steps, wait, run more steps, wait, run negative steps)
-steps = [(512, 128), (512, 0), (512, 128)]
+steps = [(512, 128), (-512, 0), (512, 128)]
 
 for step in steps:
     while left_motor.is_busy or right_motor.is_busy:

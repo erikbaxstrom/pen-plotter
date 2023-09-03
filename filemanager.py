003b9d431ad881c80f48b51055ee0ec4e43b74f3
Erik M Baxstrom <baxstrem@gmail.com>
2023-09-03 01:04:34 -0700

    remove unused debugging

--- filemanager.py
+++ filemanager.py
@@ -12,9 +12,9 @@ class FileManager:
         pass
 
     def add_to_print_file(self, data):
-        print('data', 'data')
+        # print('data', 'data')
         self.print_string += data.decode('utf-8')
-        print('print_string', self.print_string)
+        # print('print_string', self.print_string)
         return
     
     def start_print(self):

04fdad91cbf8c32ff64aac3b34b970f57beac188
Erik M Baxstrom <baxstrem@gmail.com>
2023-09-03 01:42:40 -0700

    update test gcode and clean up

--- test_main.py
+++ test_main.py
@@ -9,7 +9,6 @@ from printcontrol import PrintController
 
 
 
-
 STEPS_PER_MM = 51.2  # 2048 steps per revolution. 40 mm per revolution
 CANVAS_WIDTH = 812  # units: mm (measured as 31 31/32")
 CANVAS_HEIGHT = 889  # units: mm (measured as 35")
@@ -24,13 +23,7 @@ LEFT_MOTOR_HOME_POSITION = RIGHT_MOTOR_HOME_POSITION = 50038   # center-bottom p
 
 
 
-    
 
-
-    # print_controller.go_to_home()
-
-
-    #     print_controller.deactivate_motors()
 
 
 left_sm = StateMachine(LEFT_SM_NUMBER, pio_step, freq=10000, set_base=Pin(LEFT_SM_BASE_PIN), out_base=Pin(LEFT_SM_BASE_PIN))
@@ -50,20 +43,20 @@ try:
     print('\n--  File Manager Add File  --')
     test_print_file_string = """
 ; paint
-G1 F2300.0 X200 Y0 Z-3.3
+G1 F2300.0 X350 Y0 Z-3.3
 ; paint
-G1 F2300.0 X200 Y200 Z-3.3
+G1 F2300.0 X350 Y50 Z-3.3
 ; paint
-G1 F2300.0 X400 Y200 Z-3.3
+G1 F2300.0 X400 Y50 Z-3.3
 ; paint
 G1 F2300.0 X400 Y0 Z-3.3
 ; paint
-G1 F2300.0 X300 Y0 Z-3.3
+G1 F2300.0 X350 Y0 Z-3.3
 ;
 ;End of Gcode
 """
     file_manager.add_to_print_file(test_print_file_string.encode('utf-8'))
-    assert file_manager.print_string == test_print_file_string, f"assert file_manager.print_string == test_print_file_string"
+    assert file_manager.print_string == test_print_file_string, f"failed assert file_manager.print_string == test_print_file_string"
     print('success!')
 
     

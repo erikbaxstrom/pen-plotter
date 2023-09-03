75e2a468baabde45b214caeedf713f79314752bb
Erik M Baxstrom <baxstrem@gmail.com>
2023-09-03 01:08:18 -0700

    implement basic integration testing

--- test_main.py
+++ test_main.py
@@ -8,7 +8,9 @@ from printcontrol import PrintController
 
 
 
-STEPS_PER_MM = 2048 / 40  # 2048 steps per revolution. 40 mm per revolution
+
+
+STEPS_PER_MM = 51.2  # 2048 steps per revolution. 40 mm per revolution
 CANVAS_WIDTH = 812  # units: mm (measured as 31 31/32")
 CANVAS_HEIGHT = 889  # units: mm (measured as 35")
 
@@ -20,15 +22,14 @@ LEFT_MOTOR_DIRECTION = 1
 RIGHT_MOTOR_DIRECTION = -1
 LEFT_MOTOR_HOME_POSITION = RIGHT_MOTOR_HOME_POSITION = 50038   # center-bottom position is length * steps/mm. 
 
-    # file_manager.add_to_print_file(request.body)
 
-    # file_manager.start_print()
+
+    
 
 
     # print_controller.go_to_home()
 
-    # print('req arg', request.args.getlist('active')[0], bool(request.args.getlist('active')[0]))
-    # if request.args.getlist('active')[0] == "false":
+
     #     print_controller.deactivate_motors()
 
 
@@ -41,11 +42,55 @@ right_motor = MotorController(RIGHT_MOTOR_DIRECTION, RIGHT_MOTOR_HOME_POSITION, 
 print_controller = PrintController(CANVAS_WIDTH, CANVAS_HEIGHT, STEPS_PER_MM, left_motor, right_motor)
 file_manager = FileManager(print_controller)
 
+
 try:
-    print('tests start here')
-    # print_controller.nudge(side=request.args.getlist('motor')[0], mm=request.args.getlist('mm')[0])
+    print('----   Start of Tests   ----\n')
+
+
+    print('\n--  File Manager Add File  --')
+    test_print_file_string = """
+; paint
+G1 F2300.0 X200 Y0 Z-3.3
+; paint
+G1 F2300.0 X200 Y200 Z-3.3
+; paint
+G1 F2300.0 X400 Y200 Z-3.3
+; paint
+G1 F2300.0 X400 Y0 Z-3.3
+; paint
+G1 F2300.0 X300 Y0 Z-3.3
+;
+;End of Gcode
+"""
+    file_manager.add_to_print_file(test_print_file_string.encode('utf-8'))
+    assert file_manager.print_string == test_print_file_string, f"assert file_manager.print_string == test_print_file_string"
+    print('success!')
+
+    
+    print('\n--  File Manager Start Print  --')
+    file_manager.start_print()
+    print('success!')
+
+
+
+    print('\n--  Print Controller Go Home  --')
+    print_controller.go_to_home()
+    print('success!')
+
+
+    print('\n--  Print Controller Nudge Route --')
+    print_controller.nudge(side='left', mm=-20)
+    print_controller.nudge(side='right', mm=-20)
     print_controller.nudge(side='left', mm=20)
     print_controller.nudge(side='right', mm=20)
+    print('success!')
+
+    print('\n--  Print Controller Deactivate Motors  --')
+    print_controller.deactivate_motors()
+    print('success!')
 
 except:
-    print('broke')
+    print('\n-------   broke   -------\n')
+    raise
+
+

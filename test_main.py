b06125d404c96654f0e951c37eca5070501bed62
Erik M Baxstrom <baxstrem@gmail.com>
2023-09-03 16:56:29 -0700

    add mock state machine to test

--- test_main.py
+++ test_main.py
@@ -1,11 +1,13 @@
 from machine import Pin
 from rp2 import StateMachine
+from time import sleep
 
 from filemanager import FileManager
 from motorcontrol import MotorController
 from piostep import pio_step
 from printcontrol import PrintController
 
+from test_mock_statemachine import MockStateMachine, mock_pio_step
 
 
 
@@ -22,12 +24,15 @@ RIGHT_MOTOR_DIRECTION = -1
 LEFT_MOTOR_HOME_POSITION = RIGHT_MOTOR_HOME_POSITION = 50038   # center-bottom position is length * steps/mm. 
 
 
+MOCK = False
 
 
-
-
-left_sm = StateMachine(LEFT_SM_NUMBER, pio_step, freq=10000, set_base=Pin(LEFT_SM_BASE_PIN), out_base=Pin(LEFT_SM_BASE_PIN))
-right_sm = StateMachine(RIGHT_SM_NUMBER, pio_step, freq=10000, set_base=Pin(RIGHT_SM_BASE_PIN), out_base=Pin(RIGHT_SM_BASE_PIN))
+if MOCK:
+    left_sm = MockStateMachine(LEFT_SM_NUMBER, mock_pio_step, freq=10000, set_base=Pin(LEFT_SM_BASE_PIN), out_base=Pin(LEFT_SM_BASE_PIN))
+    right_sm = MockStateMachine(RIGHT_SM_NUMBER, mock_pio_step, freq=10000, set_base=Pin(RIGHT_SM_BASE_PIN), out_base=Pin(RIGHT_SM_BASE_PIN))
+else:
+    left_sm = StateMachine(LEFT_SM_NUMBER, pio_step, freq=10000, set_base=Pin(LEFT_SM_BASE_PIN), out_base=Pin(LEFT_SM_BASE_PIN))
+    right_sm = StateMachine(RIGHT_SM_NUMBER, pio_step, freq=10000, set_base=Pin(RIGHT_SM_BASE_PIN), out_base=Pin(RIGHT_SM_BASE_PIN))
 
 left_motor = MotorController(LEFT_MOTOR_DIRECTION, LEFT_MOTOR_HOME_POSITION, left_sm)
 right_motor = MotorController(RIGHT_MOTOR_DIRECTION, RIGHT_MOTOR_HOME_POSITION, right_sm)
@@ -74,8 +79,18 @@ G1 F2300.0 X350 Y0 Z-3.3
     print('\n--  Print Controller Nudge Route --')
     print_controller.nudge(side='left', mm=-20)
     print_controller.nudge(side='right', mm=-20)
+    if MOCK:
+        assert left_sm.result[-2] == '0 put 1024'
+        assert left_sm.result[-1] == '0 put 2216789025'
+        assert right_sm.result[-2] == '1 put 1024'
+        assert right_sm.result[-1] == '1 put 306713160'
+    
+    if not MOCK:
+        sleep(5)
     print_controller.nudge(side='left', mm=20)
     print_controller.nudge(side='right', mm=20)
+    if not MOCK:
+        sleep(5)
     print('success!')
 
     print('\n--  Print Controller Deactivate Motors  --')
@@ -84,6 +99,9 @@ G1 F2300.0 X350 Y0 Z-3.3
 
 except:
     print('\n-------   broke   -------\n')
+    if MOCK:
+        print(left_sm.result)
+        print(right_sm.result)
     raise
 
 

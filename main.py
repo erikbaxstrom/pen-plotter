97c4f83359074391c9939cac147fd6a337d2edd0
Erik M Baxstrom <baxstrem@gmail.com>
2023-09-03 23:24:04 -0700

    move refactored code to main for end-to-end test

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+from machine import Pin
 from microdot import Microdot, Response, send_file
+from rp2 import StateMachine
+from time import sleep
 
-from filemanager import FileManager
-from printcontrol import PrintController
 from connectionmanager import connect_wifi
+from filemanager import FileManager
+from motorcontrol import MotorController
+from piostep import pio_step
+from printcontrol import PrintController
 
 
-STEPS_PER_MM = 2048 / 40  # 2048 steps per revolution. 40 mm per revolution
+
+STEPS_PER_MM = 51.2  # 2048 steps per revolution. 40 mm per revolution
 CANVAS_WIDTH = 812  # units: mm (measured as 31 31/32")
 CANVAS_HEIGHT = 889  # units: mm (measured as 35")
+
+LEFT_SM_BASE_PIN = 6
+RIGHT_SM_BASE_PIN = 2
+LEFT_SM_NUMBER = 0
+RIGHT_SM_NUMBER = 1
+LEFT_MOTOR_DIRECTION = 1
+RIGHT_MOTOR_DIRECTION = -1
+LEFT_MOTOR_HOME_POSITION = RIGHT_MOTOR_HOME_POSITION = 50038   # center-bottom position is length * steps/mm. 
 
 
 app = Microdot()
 Response.default_content_type = 'text/html'
+
+
+
 
 @app.route('/')
 def index(request):
@@ -51,9 +68,17 @@ def deactivate_motors(request):
     return 'woohoo', 200
 
 
+left_sm = StateMachine(LEFT_SM_NUMBER, pio_step, freq=10000, set_base=Pin(LEFT_SM_BASE_PIN), out_base=Pin(LEFT_SM_BASE_PIN))
+right_sm = StateMachine(RIGHT_SM_NUMBER, pio_step, freq=10000, set_base=Pin(RIGHT_SM_BASE_PIN), out_base=Pin(RIGHT_SM_BASE_PIN))
+
+left_motor = MotorController(LEFT_MOTOR_DIRECTION, LEFT_MOTOR_HOME_POSITION, left_sm)
+right_motor = MotorController(RIGHT_MOTOR_DIRECTION, RIGHT_MOTOR_HOME_POSITION, right_sm)
+
+print_controller = PrintController(CANVAS_WIDTH, CANVAS_HEIGHT, STEPS_PER_MM, left_motor, right_motor)
+file_manager = FileManager(print_controller)
+
 try:
-    print_controller = PrintController(CANVAS_WIDTH, CANVAS_HEIGHT, STEPS_PER_MM)
-    file_manager = FileManager(print_controller)
+
     print('running')
     ip = connect_wifi()
     app.run(host=ip, port=80, debug=True)

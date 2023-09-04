39a7ea6451946ae8c562951f9fcb7c2d0bdb00e0
Erik M Baxstrom <baxstrem@gmail.com>
2023-09-04 00:33:33 -0700

    experiment with json configuration

--- unused_experiment_json_config.py
+++ unused_experiment_json_config.py
@@ -1,42 +1,29 @@
 import ujson
-# import re
-
-# with open('configuration.json', 'r') as file:
-#     jsonString = file.read()
-#     # json_string = re.sub(r'(?<!^)(?=[A-Z])', '_', jsonString).lower()
-#     # print(type(jsonString))
-#     parsed = ujson.loads(jsonString)
-#     print(parsed)
 
 
+class Configuration():
+
+    def __init__(self, filename):
+        raw_config = self.fetch_configuration(filename)
+        self.configuration = self.parse_configuration(raw_config)
 
 
+    def fetch_configuration(self, filename):
+        with open(filename, 'r') as file:
+            jsonString = file.read()
+            raw_config = ujson.loads(jsonString)
+            return raw_config
 
 
+    def parse_configuration(self, raw_config):
+        configuration = {}
+        for header, section in raw_config.items():
+            for key, value in section.items():
+                configuration[key] = value
+        return configuration
 
-# new code
+FILENAME = 'configuration.json'
 
-def fetch_configuration(filename):
-    with open(filename, 'r') as file:
-        jsonString = file.read()
-        raw_config = ujson.loads(jsonString)
-        return raw_config
+configuration = Configuration(FILENAME)
 
-
-
-def parse_configuration(raw_config):
-
-    configuration = {}
-    print('raw_config', raw_config)
-    for header, section in raw_config.items():
-        print('this is header, section', header, section)
-        for key, value in section:
-            print('this is key,val', key, value)
-            configuration[key] = value
-    print(configuration)
-    return configuration
-
-
-raw_config = fetch_configuration('configuration.json')
-# print('jsonstring', type(jsonString), jsonString[0:10])
-configuration = parse_configuration(raw_config)
+print(configuration.configuration['name1.1'])
